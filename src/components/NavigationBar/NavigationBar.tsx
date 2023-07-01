c0b95786d26a5f1eeb29fe314692801b1bb23316
IconicLuck <uhasib959@gmail.com>
2023-07-01 23:10:54 +0600

    added icons on navigation bar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,48 +1,53 @@
 import { NavLink } from "react-router-dom";
 import './NavigationBar.css'
+import { FaHome, FaSearch } from "react-icons/fa";
+import { MdFavorite, MdPlaylistAddCircle } from "react-icons/md";
 
 const NavigationBar = () => {
+
+    // TODO: Implementation of React Icons
+
     return (
         <div className="bg-primary d-flex flex-column h-100 justify-content-center align-items-center gap-3 border border-primary navigation-container">
             <NavLink
                 to='/'
                 className={({ isActive }) =>
                     isActive
-                        ? "bg-secondary-subtle p-2 text-black fw-bold nav-link rounded fs-5"
-                        : "text-white fw-bold nav-link fs-5"
+                        ? "bg-secondary-subtle p-2 text-black fw-bold nav-link rounded fs-5 d-flex align-items-center gap-2"
+                        : "text-white fw-bold nav-link fs-5 d-flex align-items-center gap-2"
                 }
             >
-                Home
+                <FaHome></FaHome> Home
             </NavLink>
             <NavLink
                 to='/search'
                 className={({ isActive }) =>
                     isActive
-                        ? "bg-secondary-subtle p-2 text-black fw-bold nav-link rounded fs-5"
-                        : "text-white fw-bold nav-link fs-5"
+                        ? "bg-secondary-subtle p-2 text-black fw-bold nav-link rounded fs-5 d-flex align-items-center gap-2"
+                        : "text-white fw-bold nav-link fs-5 d-flex align-items-center gap-2"
                 }
             >
-                Search
+                <FaSearch></FaSearch> Search
             </NavLink>
             <NavLink
                 to='/favorites'
                 className={({ isActive }) =>
                     isActive
-                        ? "bg-secondary-subtle p-2 text-black fw-bold nav-link rounded fs-5"
-                        : "text-white fw-bold nav-link fs-5"
+                        ? "bg-secondary-subtle p-2 text-black fw-bold nav-link rounded fs-5 d-flex align-items-center gap-2"
+                        : "text-white fw-bold nav-link fs-5 d-flex align-items-center gap-2"
                 }
             >
-                Favorites
+               <MdFavorite></MdFavorite> Favorites
             </NavLink>
             <NavLink
                 to='/playlists'
                 className={({ isActive }) =>
                     isActive
-                        ? "bg-secondary-subtle p-2 text-black fw-bold nav-link rounded fs-5"
-                        : "text-white fw-bold nav-link fs-5"
+                        ? "bg-secondary-subtle p-2 text-black fw-bold nav-link rounded fs-5 d-flex align-items-center gap-2"
+                        : "text-white fw-bold nav-link fs-5 d-flex align-items-center gap-2"
                 }
             >
-                Playlists
+               <MdPlaylistAddCircle></MdPlaylistAddCircle> Playlists
             </NavLink>
         </div>
     );
